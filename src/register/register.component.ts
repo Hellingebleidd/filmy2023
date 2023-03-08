dc2d64cc263ab5ef43664f224f94f3ff413d750f
Zuzanka <hellingebleidd@gmail.com>
2023-03-08 10:22:43 +0100

    validators

--- src/register/register.component.ts
+++ src/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { User } from 'src/entities/user';
 import { UsersService } from 'src/services/users.service';
+import * as zxcvbn from 'zxcvbn'
 
 @Component({
   selector: 'app-register',
@@ -13,10 +14,25 @@ export class RegisterComponent {
   // toto tu je namiesto konstruktoru
   usersService = inject(UsersService);
 
+  pswdMessage=''
+
+  //sipkova funkcia sa spracuje ako posledna, preto ju musim uviest PRED miestom kde ju pouzivam
+  pswdValidator=(control: AbstractControl):ValidationErrors|null =>{
+    const pswd = control.value
+    const res = zxcvbn(pswd)
+    this.pswdMessage = 'password strength: '+res.score + '/4, crackable in '+res.crack_times_display.offline_slow_hashing_1e4_per_second
+    return res.score <3 ? {'weakPassword': this.pswdMessage} : null
+  }
+
   registerForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl<string>('', { nonNullable: true }),
-    password: new FormControl(''),
+    name: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(3)],
+    }),
+    email: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email, Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,}$")],
+    }),
+    password: new FormControl('', this.pswdValidator),
     password2: new FormControl(''),
   });
 
@@ -43,4 +59,10 @@ export class RegisterComponent {
     );
     this.usersService.register(newUser).subscribe();
   }
+
+  printError(fc: FormControl){
+    return JSON.stringify(fc.errors)
+  }
+
+
 }
