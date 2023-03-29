3fb4c422be7f851df8996e36a18c7d6aebe3395b
Zuzanka <hellingebleidd@gmail.com>
2023-03-29 09:08:30 +0200

    ws v environmente

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment ={
-    websocket_url: 'http://ws.filmy.sk/',  //nejake produkcne adresy nasadene
+    websocket_url: 'ws://ws.filmy.sk/',  //nejake produkcne adresy nasadene
     userServer_url: 'http://filmy.sk/'
 }
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment ={
-    websocket_url: 'http://localhost:8080/ws',
+    websocket_url: 'ws://localhost:8080/ws',
     userServer_url: 'http://localhost:8080/'
 }
