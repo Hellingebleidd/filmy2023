6e3f8f8a2fde7af0f51fcab6c6d7711d10d08f63
Zuzanka <hellingebleidd@gmail.com>
2023-04-05 13:57:50 +0200

    herci a reziser z nasej DB

--- src/modules/films/film-detail/film-detail.component.ts
+++ src/modules/films/film-detail/film-detail.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { environment } from 'src/environment/environment';
 import { FilmsService, omdbFilm } from '../films.service';
+import { Clovek } from 'src/entities/clovek';
+import { Postava } from 'src/entities/postava';
 
 @Component({
   selector: 'app-film-detail',
@@ -10,26 +12,33 @@ import { FilmsService, omdbFilm } from '../films.service';
 })
 export class FilmDetailComponent implements OnChanges{
 
-  // @Input() imdbId?: string
-  @Input() imdbFilm?: omdbFilm
 
-  url: string = environment.omdbApi_url
+  @Input() imdbFilm?: omdbFilm
+  @Input() rezia?: Clovek[]
+  @Input() postavy?: Postava[]
+
+
   movieData?: omdbFilm
-  id?:string
+  reziser=''
+  herci: string[] = []
+  ludia?: Clovek[]
+  obsadenie=''
 
   constructor(private http: HttpClient, private filmsService: FilmsService){}
-
-  // ngOnChanges(changes: SimpleChanges): void {
-    // this.id=this.imdbId || ''
-    // console.log("id: ", this.id);
-    // if(this.id) this.http.get(this.url+this.id).subscribe(data=>this.movieData=data)
-    // this.filmsService.getOmdbFilm(this.url+this.id).subscribe(data=>this.movieData=data)
-  // }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.movieData=this.imdbFilm
 
+    if(this.rezia){
+      this.rezia.map(r => this.reziser=r.krstneMeno+' ' + (r.stredneMeno? r.stredneMeno+' '+r.priezvisko : r.priezvisko))
+
+    if(this.postavy){
+      this.ludia= this.postavy.map(p => p.herec)
+      this.herci = this.ludia.map(herec=>herec.krstneMeno+' ' + (herec.stredneMeno? herec.stredneMeno+' '+herec.priezvisko : herec.priezvisko))
+      this.obsadenie = this.herci.join(', ')
     }
+  }
+  }
 
   // ngOnInit(): void {
   //   const id = this.imdbId
